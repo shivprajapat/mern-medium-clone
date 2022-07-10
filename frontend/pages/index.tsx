a2c900dae933fedd4f57ebcc8987f5e362cb1550
shivprajapat <shivkumarprajapat232@gmail.com>
2022-07-10 09:52:55 +0530

    frontend post card showing sanity api

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -20,16 +20,25 @@ const Home: NextPage = ({ posts }: Props) => {
       <Header />
       <Banner />
       {/* Post */}
-
-      {
-        posts.map((post) => (
-          <Link key={post._id} href={`/post/${post.slug.current}`}>
-            <div>
-              <img src={urlFor(post.mainImage).url()} alt="" />
-            </div>
-          </Link>
-        ))
-      }
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 py-12 md:py-6">
+        {
+          posts.map((post) => (
+            <Link key={post._id} href={`/post/${post.slug.current}`}>
+              <div className='border  border-lg group  overflow-hidden cursor-pointer'>
+                <img className='w-full object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out' src={urlFor(post.mainImage).url()} alt="" />
+                <div className="flex justify-between p-5 bg-white">
+                  <div>
+                    <p className='text-lg font-bold'>{post.title}</p>
+                    <p className='text-xs'>{post.description} by <span className='font-bold text-pink-500'>{post.title}</span> </p>
+                  </div>
+                  <img className="h-12 w-12 rounded-full" src={urlFor(post.mainImage).url()} alt={post.title} />
+                </div>
+              </div>
+            </Link>
+          ))
+        }
+      </div>
+      {/* ./Post */}
     </div>
   )
 }
